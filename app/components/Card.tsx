7885aed4a6052fffaf7e1e25635555890945938b
sadafshahab12~ <sadafshahab123@yahoo.com>
2024-09-26 08:30:48 +0500

    next js assignment 3

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,39 +1,17 @@
 import React from "react";
 import Contactme from "./Contactme";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
-import { faReact } from "@fortawesome/free-brands-svg-icons";
-const Card = () => {
+
+const Card = (props) => {
   return (
     <>
       <div className="bg-blue-300 my-5 rounded-2xl p-5 text-center flex gap-4 flex-col h-[22rem]">
         <FontAwesomeIcon
-          icon={faReact}
+          icon={props.icon}
           className="grid lg:text-6xl md:text-5xl sm:text-4xl text-3xl"
         />
-        <h5 className="grid lg:text-3xl text-2xl font-bold">
-          Front End Development
-        </h5>
-        <p className="grid lg:text-[1.2rem] md:text-[1rem]">
-          I specialize in front-end development, utilizing Next.js, React, and
-          Tailwind CSS.My focus is on creating responsive and scalable web
-          applications that enhance user experience.
-        </p>
-        <Contactme />
-      </div>
-      <div className="bg-blue-300 my-5 rounded-2xl p-5 text-center flex gap-4 flex-col h-[22rem]">
-        <FontAwesomeIcon
-          icon={faLaptopCode}
-          className="grid lg:text-6xl md:text-5xl sm:text-4xl text-3xl"
-        />
-        <h5 className="grid lg:text-3xl text-2xl font-bold">
-          Front End Development
-        </h5>
-        <p className="grid lg:text-[1.2rem] md:text-[1rem]">
-          I specialize in front-end development, utilizing HTML, CSS, and
-          JavaScript. My focus is on creating responsive and scalable web
-          applications that enhance user experience.
-        </p>
+        <h5 className="grid lg:text-3xl text-2xl font-bold">{props.title}</h5>
+        <p className="grid lg:text-[1.2rem] md:text-[1rem]">{props.info}</p>
         <Contactme />
       </div>
     </>
--- app/service/page.tsx
+++ app/service/page.tsx
@@ -2,13 +2,22 @@
 import React from "react";
 import Card from "../components/Card";
 
-
+import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
 const Service = () => {
   return (
     <>
       <div className="mx-w-[1440px] h-[100%] grid lg:grid-cols-2 sm:grid-cols-1 gap-8 p-10 bg-black">
-        <Card/>
-     
+        <Card
+          icon={faReact}
+          title="Front End Development"
+          info="I specialize in front-end development, utilizing Next.js, React, and Tailwind CSS.My focus is on creating responsive and scalable web applications that enhance user experience."
+        />
+        <Card
+          icon={faLaptopCode}
+          title="Front End Development"
+          info="I specialize in front-end development, utilizing HTML, CSS, and JavaScript. My focus is on creating responsive and scalable web applications that enhance user experience."
+        />
       </div>
     </>
   );
